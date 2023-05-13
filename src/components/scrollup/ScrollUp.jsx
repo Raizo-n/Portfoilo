8d9fbda3e755729a63f2e9e75dc8dd149c0149fd
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-13 14:42:35 +0900

    format

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -9,6 +9,7 @@ const ScrollUp = () => {
     if (this.scrollY >= 560) scrollup.classList.add("show-scroll");
     else scrollup.classList.remove("show-scroll");
   });
+
   return (
     <a href="#" className="scrollup">
       <i className="uil uil-arrow-up icon"></i>
