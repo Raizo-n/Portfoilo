33e74d2ea3dbd5d02947e559a11e607dfba93733
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-17 10:33:46 -0700

    add: home in components

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home_social">
+      <a href="https://www.linkedin.com" className="home_social-icon" target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a href="https://github.com/Raizo-n" className="home_social-icon" target="_blank">
+        <i class="uil uil-times"></i>
+      </a>
+      <a href="" className="home_social-icon" target="_blank"></a>
+    </div>
+  );
+};
+
+export default Social;
