1c8f715fe220bd2fb9ac4a8be119eb559abbf2f8
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-17 21:14:43 +0900

    add: home.scss for large device

---
+++ src/components/home/ScrollDown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ScrollDown = () => {
+  return (
+    <div className="scroll">
+        <a href="#about" className='scroll-button'></a>
+    </div>
+  )
+}
+
+export default ScrollDown
