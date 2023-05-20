697306ebe474166285f8625edece5c1bfbc9cf7d
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-20 17:41:23 +0900

    add: first commit of contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./contact.scss";
+
+const Contact = () => {
+  return (
+    <section className="contact section" id="contact">
+      <h2 className="section_title"></h2>
+      <span className="section_subtitle"></span>
+    </section>
+  );
+};
+
+export default Contact;
