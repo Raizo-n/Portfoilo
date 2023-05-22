36c96fa921d12bb50433fc8e3c647d7a03958a89
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-22 02:02:04 -0700

    add: style of contact on the left side

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,32 +7,27 @@ const Contact = () => {
       <h2 className="section_title">Get in Touch</h2>
       <span className="section_subtitle">Contact Me</span>
 
-      <div className="contact_container container grid">
+      <div className="contact-container container grid">
         <div className="content">
           <h3 className="title">Talk to me</h3>
           <div className="info">
-            <div className="card">
-              <i className="bx bx-mail-send contact__card-icon"></i>
-              <h3 className="title"></h3>
-              <span className="data"></span>
-            </div>
 
             {/* Email */}
             <div className="card">
-              <i className="bx bx contact__card-icon"></i>
-              <h3 className="title">Email</h3>
+              <i className="bx bx-mail-send icon"></i>
+              <h3 className="formtitle">Email</h3>
               <span className="data">raizonakada@gmail.com</span>
-              <a href="mailto:raizonakada@gmail.com.com" className="button">
+              <a href="mailto:raizonakada@gmail.com.com" className="contact-button">
                 Write me <i className="bx bx-right-arrow-alt button-icon"></i>
               </a>
             </div>
 
             {/* Whatsapp */}
             <div className="card">
-              <i className="bx bxl-whatsapp contact__card-icon"></i>
-              <h3 className="title">Whatsapp</h3>
+              <i className="bx bxl-whatsapp icon"></i>
+              <h3 className="formtitle">Whatsapp</h3>
               <span className="data">604-657-4679</span>
-              <a href="" className="button">
+              <a href="" className="contact-button">
                 Write me <i className="bx bx-right-arrow-alt button-icon"></i>
               </a>
             </div>
