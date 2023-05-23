47a8bd5817aaeb6401a737298706ff7c4e183872
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-23 02:18:27 -0700

    add: footer content

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
 import "./footer.scss";
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-        <div className="footer-container container">
-        </div>
-    </footer>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container container">
+        <h1 className="title">Raizo Nakada</h1>
 
-export default Footer
+        <ul className="list">
+          <li>
+            <a href="#about" className="link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className="link">
+              Project
+            </a>
+          </li>
+        </ul>
+
+        <span className="copy">&#169; Raizo Nakada All rights reserved</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
