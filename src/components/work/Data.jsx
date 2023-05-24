8102bcd0942e3db9fcf3d2ca07f412090511dc26
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-24 11:07:25 -0700

    add: first commit

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,22 @@
+export const projectData = [
+    {
+        id: 1,
+        image: Work1,
+        title: 'Web design',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'Web design',
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: 'Web design',
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: 'Web design',
+    },
+]
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./work.scss";
+import Works from "./Works";
 
 const Work = () => {
   return (
     <section className="work section" id="portfolio">
       <h2 className="section_title">Portfolio</h2>
       <span className="section_subtitle">Most recent works</span>
+      <Works />
     </section>
   );
 };
