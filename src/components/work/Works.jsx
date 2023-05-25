64da3ad408d7c850243de3ab744554d51d1a9883
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-25 16:36:54 -0700

    feat: zoom the img of pf on hover

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -7,7 +7,9 @@ const Works = () => {
       {projectData.map((item) => {
         return (
           <div className="card" key={item.id}>
-            <img src={item.image} alt="" className="img" />
+            <div className="img-box">
+              <img src={item.image} alt="" className="img" />
+            </div>
             <h3 className="title">{item.title}</h3>
             <a href="#" className="work-button">
               Demo <i className="bx bx-right-arrow-alt icon"></i>
