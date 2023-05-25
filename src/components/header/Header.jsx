4647fb4824a04891d9f6da24fd4a88e04657e404
“Raizo-n” <sanpincha0826@gmail.com>
2023-05-25 11:27:09 -0700

    fix: small bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./header.scss";
 
 const Header = () => {
-  
+
   // Toggle Menu
   const [Toggle, showMenu] = useState(false);
 
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,7 @@ import Backend from './Backend'
 
 const Skills = () => {
   return (
-    <section className="skills section" id='#skills'>
+    <section className="skills section" id='skills'>
         <h2 className="section_title">Skills</h2>
         <span className="section_subtitle">My technical level</span>
         <div className="skills_container container grid">
