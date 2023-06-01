b33852e388c4c47a486d087535bd2793a456ef74
“Raizo-n” <sanpincha0826@gmail.com>
2023-06-01 16:34:15 +0900

    add: hand icon

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
+import hand from "../../assets/433.png";
 
 const Data = () => {
   return (
     <div className="data">
-        <h1 className="title">
-            Raizo Nakada
-            {/* <svg></svg> */}
-        </h1>
-        <h3 className="subtitle">Front-end Developer</h3>
-        <p className="description">Based in Vancouver</p>
-        <a href="#contact" className='button button--flex hello-btn'>Say Hello</a>
+      <h1 className="title">
+        Raizo Nakada
+        <img src={hand} alt="" className="hand-icon" />
+      </h1>
+      <h3 className="subtitle">Front-end Developer</h3>
+      <p className="description">Based in Vancouver</p>
+      <a href="#contact" className="button button--flex hello-btn">
+        Say Hello
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Data
+export default Data;
