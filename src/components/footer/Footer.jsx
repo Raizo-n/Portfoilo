a8a2fafb2a1e9d634d8ef2b383b78d0e914c2d63
“Raizo-n” <sanpincha0826@gmail.com>
2023-06-15 15:35:52 +0900

    add: menuArr and menu

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import "./footer.scss";
 
 const Footer = () => {
+  const menuArr = [
+    { menu: "About", link: "#about" },
+    { menu: "Skills", link: "#skills" },
+    { menu: "Project", link: "#portfolio" },
+    { menu: "Contact", link: "#contact" },
+  ];
+
+  const menu = menuArr.map((item) => (
+    <li>
+      <a href={item.link} className="link">
+        {item.menu}
+      </a>
+    </li>
+  ));
+
   return (
     <footer className="footer">
       <div className="footer-container container">
         <h1 className="title">Raizo Nakada</h1>
-
-        <ul className="list">
-          <li>
-            <a href="#about" className="link">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#portfolio" className="link">
-              Project
-            </a>
-          </li>
-        </ul>
-
-        <span className="copy">&#169; Raizo Nakada 2023 All rights reserved</span>
+        <ul className="list">{menu}</ul>
+        <span className="copy">
+          &#169; Raizo Nakada 2023 All rights reserved
+        </span>
       </div>
     </footer>
   );
