d8638a1f4eb954b3b8fa6235200167ce6953bcd1
“Raizo-n” <sanpincha0826@gmail.com>
2023-06-23 22:19:54 -0700

    fix: typo from scrollup to scrollUp

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -3,11 +3,11 @@ import "./scrollUp.scss";
 
 const ScrollUp = () => {
   window.addEventListener("scroll", function() {
-    const scrollup = document.querySelector(".scrollup");
+    const scrollUp = document.querySelector(".scrollup");
     // when the scroll is higher than 560 viewport height,
     // add the show-scroll class to a tag with the scroll
-    if (this.scrollY >= 560) scrollup.classList.add("show-scroll");
-    else scrollup.classList.remove("show-scroll");
+    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
   });
 
   return (
