bcbb22b581aa46f8bb7e9031be5dd4dfd835e706
“Raizo-n” <sanpincha0826@gmail.com>
2023-07-03 17:53:37 -0700

    style: small adjustment of the design on 768px device

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -4,7 +4,7 @@ import Works from "./Works";
 
 const Work = () => {
   return (
-    <section className="work section" id="project">
+    <section className="work container section" id="project">
       <h2 className="section_title">Project</h2>
       <span className="section_subtitle">Most recent works</span>
       <Works />
