aff03918159eb237db34b144de7fc83a3ae1b641
“Raizo-n” <sanpincha0826@gmail.com>
2023-07-13 18:25:53 +0900

    update: add backEndSkillArr and backEndSkill in order to make it easy to read code

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,37 +1,29 @@
-
-import React from 'react'
+import React from "react";
 
 const Backend = () => {
-    return (
-      <div className="content">
-        <h3 className="title">Back-end skills</h3>
-        <div className="box">
-          <div className="group">
-            <div className="data">
-              <i class="bx bxl-nodejs"></i>
-              <div>
-                <h3 className="name">Node JS</h3>
-                {/* <span className="level">Basic</span> */}
-              </div>
-            </div>
-            <div className="data">
-              <i class="bx bxl-mongodb"></i>
-              <div>
-                <h3 className="name">MongoDB</h3>
-                {/* <span className="level">Basic</span> */}
-              </div>
-            </div>
-            <div className="data">
-              <i class="bx bx-badge-check"></i>
-              <div>
-                <h3 className="name">My SQL</h3>
-                {/* <span className="level">Basic</span> */}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
+  const backEndSkillArr1 = [
+    { icon: "bx bxl-nodejs", skill: "Node JS" },
+    { icon: "bx bxl-mongodb", skill: "MongoDB" },
+    { icon: "bx bxl-badge-check", skill: "MySQL" },
+  ];
 
-export default Backend
+  const backEndSkill1 = backEndSkillArr1.map((item) => (
+    <div className="data">
+      <i class={item.icon}></i>
+      <div>
+        <h3 className="name">{item.skill}</h3>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="content">
+      <h3 className="title">Back-end skills</h3>
+      <div className="box">
+        <div className="group">{backEndSkill1}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
