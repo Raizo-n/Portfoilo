6e6d23e4fd52e89d2628ec406ea23b61babb0830
“Raizo-n” <sanpincha0826@gmail.com>
2023-07-16 22:58:53 -0700

    fix: typo

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -3,7 +3,7 @@ import "./scrollUp.scss";
 
 const ScrollUp = () => {
   window.addEventListener("scroll", function() {
-    const scrollUp = document.querySelector(".scrollup");
+    const scrollUp = document.querySelector(".scrollUp");
     // when the scroll is higher than 560 viewport height,
     // add the show-scroll class to a tag with the scroll
     if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
