204cc37226ad5f437a75d8f1b9cd873c2464cb2d
“Raizo-n” <sanpincha0826@gmail.com>
2023-07-22 18:43:51 +0900

    update: refactor the code of displaying skillset

---
+++ src/components/skills/DisplaySkill.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DisplaySkill = ({skillSet}) => {
+  return (
+    skillSet.map((item) => (
+        <div className="data">
+          <i class={item.icon}></i>
+          <div>
+            <h3 className="name">{item.skill}</h3>
+          </div>
+        </div>
+      )
+  ))
+}
+
+export default DisplaySkill
--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DisplaySkill from "./DisplaySkill";
 
 const Frontend = () => {
   const frontEndSkillArr1 = [
@@ -13,30 +14,36 @@ const Frontend = () => {
     { icon: "bx bxl-tailwind-css", skill: "Tailwind" },
   ];
 
-  const frontEndSkill1 = frontEndSkillArr1.map((item) => (
-    <div className="data">
-      <i class={item.icon}></i>
-      <div>
-        <h3 className="name">{item.skill}</h3>
-      </div>
-    </div>
-  ));
+  // const frontEndSkill1 = frontEndSkillArr1.map((item) => (
+  //   <div className="data">
+  //     <i class={item.icon}></i>
+  //     <div>
+  //       <h3 className="name">{item.skill}</h3>
+  //     </div>
+  //   </div>
+  // ));
 
-  const frontEndSkill2 = frontEndSkillArr2.map((item) => (
-    <div className="data">
-      <i class={item.icon}></i>
-      <div>
-        <h3 className="name">{item.skill}</h3>
-      </div>
-    </div>
-  ));
+  // const frontEndSkill2 = frontEndSkillArr2.map((item) => (
+  //   <div className="data">
+  //     <i class={item.icon}></i>
+  //     <div>
+  //       <h3 className="name">{item.skill}</h3>
+  //     </div>
+  //   </div>
+  // ));
 
   return (
     <div className="content">
       <h3 className="title">Front-end skills</h3>
       <div className="box">
-        <div className="group">{frontEndSkill1}</div>
-        <div className="group">{frontEndSkill2}</div>
+        <div className="group">
+          <DisplaySkill skillSet={frontEndSkillArr1} />
+        </div>
+        <div className="group">
+          <DisplaySkill skillSet={frontEndSkillArr2} />
+        </div>
+        {/* <div className="group">{frontEndSkill1}</div>
+        <div className="group">{frontEndSkill2}</div> */}
       </div>
     </div>
   );
