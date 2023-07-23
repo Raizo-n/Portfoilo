ea5201f8bf5b2cbd2b6a62111021684536af6381
“Raizo-n” <sanpincha0826@gmail.com>
2023-07-23 17:43:11 +0900

    format

--- src/components/skills/DisplaySkill.jsx
+++ src/components/skills/DisplaySkill.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const DisplaySkill = ({skillSet}) => {
-  return (
-    skillSet.map((item) => (
-        <div className="data">
-          <i class={item.icon}></i>
-          <div>
-            <h3 className="name">{item.skill}</h3>
-          </div>
-        </div>
-      )
-  ))
-}
+const DisplaySkill = ({ skillSet }) => {
+  return skillSet.map((item) => (
+    <div className="data">
+      <i class={item.icon}></i>
+      <div>
+        <h3 className="name">{item.skill}</h3>
+      </div>
+    </div>
+  ));
+};
 
-export default DisplaySkill
+export default DisplaySkill;
