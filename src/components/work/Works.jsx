641a6cdeca962ee225f7c88872dfce4e280d6a19
“Raizo-n” <sanpincha0826@gmail.com>
2023-08-21 13:32:53 +0900

    add: description in the movie app

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -138,16 +138,13 @@ const Works = () => {
             <img src={MovieAppImg} alt="" className="img" />
             <h3 className="title">Movie App</h3>
             <p className="project-desc">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quos
-              aspernatur amet quibusdam, rem velit repellendus ut mollitia harum
-              dolorum quis et aut dolor inventore repellat quo molestias
-              consectetur commodi.
+              This app allows you to search for a movie or show. You can get detailed information of it by clicking each movie or show.
+              I built this app using OMDb API.
             </p>
             <div className="skill">
               <span className="tech-title">Technology</span>
-              <span className="item">HTML</span>
-              <span className="item">CSS</span>
-              <span className="item">JavaScript</span>
+              <span className="item">SCSS</span>
+              <span className="item">React</span>
             </div>
           </div>
         </div>
